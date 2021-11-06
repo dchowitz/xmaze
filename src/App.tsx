97e05abb96facfead018ce7aab0d81eba73d4f2b
dchowitz <denny.christochowitz@gmail.com>
2021-11-06 21:17:49 +0100

    show build date and git hash in footer

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,15 @@ export default function App() {
           demoFac={(ctx) => demoMask(ctx, xmas, () => setShowFooter(true))}
         />
       </div>
-      <footer style={{ visibility: showFooter ? "visible" : "hidden" }}>
-        <div>
+      <footer>
+        <div style={{ visibility: showFooter ? "visible" : "hidden" }}>
           ...&nbsp;from&nbsp;
           <a href="https://github.com/dchowitz/xmaze">dchowitz</a> (aka foobar
           (fka prensen))
         </div>
+        <pre>
+          build {import.meta.env.VITE_DATE} ({import.meta.env.VITE_GITHASH})
+        </pre>
       </footer>
     </div>
   );
